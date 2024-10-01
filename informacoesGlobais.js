b661fc84bf6b2d92872852e2e534a199d6f0a5ad
kerollin0405 <130009050+kerollin0405@users.noreply.github.com>
2024-09-30 21:18:23 -0300

    Update informacoesGlobais.js
    ......

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,7 +1,7 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 async function vizualizarInformacoesGlobais() {
-console.log(url);
-const dados = await res.json()
-    console.log(dados);
+  const res = await fetch(url)
+  const dados await res.json()
+  console.log(dados.tempo_medio);
 }
+
 vizualizarInformacoesGlobais()
